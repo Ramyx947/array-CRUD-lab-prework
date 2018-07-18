7e4991a65683f4b199cda35419ea29c077283756
Ramona M <r1mari@yahoo.com>
2018-07-18 12:54:38 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
-def create_an_empty_array
-[]
+def create_an_empty_array(array)
+array=[]
 end
 
 def create_an_array
-  array=Array.new
+  array = []
 end
 
 def add_element_to_end_of_array(array, element)
